7764c66331ad22611cab36fe23e24997bb0e6715
Darwin Yuan <darwin.yuan@hotmail.com>
2021-06-30 05:07:25 +0800

    refactored concept

--- include/l0-infra/array/holder/ScopedViewDataHolder.h
+++ include/l0-infra/array/holder/ScopedViewDataHolder.h
@@ -10,16 +10,14 @@
 #include <l0-infra/array/holder/detail/ArrayLikeViewDataHolder.h>
 #include <l0-infra/array/holder/detail/ScopedViewDataHolderInterface.h>
 #include <l0-infra/base/BitSet.h>
+#include <l0-infra/array/holder/detail/ViewDataHolder.h>
 
 namespace holder {
     template<__cOnCePt(SimpleRangedArrayLike) ARRAY, typename SUB_TYPE, bool ORDERED>
-    struct ScopedViewDataHolder {
-        constexpr static auto IS_CONST = std::is_const_v<ARRAY> || ARRAY::CONST;
-        using ArrayType = std::decay_t<ARRAY>;
-        using ObjectType = std::conditional_t<IS_CONST, std::add_const_t<typename ArrayType::ObjectType>, typename ArrayType::ObjectType>;
-        using SizeType = typename ArrayType::SizeType;
-        constexpr static SizeType MAX_SIZE = ArrayType::MAX_SIZE;
-
+    struct ScopedViewDataHolder : detail::ViewDataHolder<ARRAY> {
+        using Parent = detail::ViewDataHolder<ARRAY>;
+        using typename Parent::ObjectType;
+        using typename Parent::SizeType;
         constexpr static bool IS_ORDERED = ORDERED;
         using BitMap = typename ARRAY::BitMap;
 
